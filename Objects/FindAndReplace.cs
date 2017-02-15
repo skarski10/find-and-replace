85a1eeac1b730f3d7c3d2dba319a3c84447dac36
Kory Skarbek and Sam Kirsch <kory+sam@>
2017-02-15 11:07:53 -0800

    initialize the base files and directories

---
+++ Objects/FindAndReplace.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace FindAndReplaceApp.Objects
+{
+    public class FindAndReplace
+    {
+        
+    }
+}
---
+++ Tests/FindAndReplaceTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+using System.Collections.Generic;
+using FindAndReplaceApp.Objects;
+
+namespace FindAndReplaceAppTest
+{
+    public class FindAndReplaceTest
+    {
+        [Fact]
+        public void FirstTestMethod_Functionality_Result()
+        {
+            // Put stuff here
+        }
+    }
+}
