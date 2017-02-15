fd41d2e703c1a94808049365860625a2b952312f
Kory Skarbek and Sam Kirsch <kory+sam@>
2017-02-15 14:08:08 -0800

    add final find and replace test pass final test add front end functions link pages, finalize program

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -6,9 +6,17 @@ namespace FindAndReplaceApp
 {
     public class HomeModule : NancyModule
     {
-        // public HomeModule()
-        // {
-        //
-        // }
+        public HomeModule()
+        {
+            Get["/"] = _ => {
+                return View["index.cshtml"];
+            };
+        
+            Post["/"] = _ => {
+                FindAndReplace newFindAndReplace = new FindAndReplace(Request.Form["sentence"], Request.Form["replace"], Request.Form["new"]);
+                string finalResult = newFindAndReplace.ReplaceFindWord();
+                return View["index.cshtml", finalResult];
+            };
+        }
     }
 }
