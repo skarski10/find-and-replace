714bb8154fcf7d730c14145bd640724c2ee83227
Kory Skarbek and Sam Kirsch <kory+sam@>
2017-02-15 11:40:13 -0800

    add and pass expanded constructor test

--- Objects/FindAndReplace.cs
+++ Objects/FindAndReplace.cs
@@ -7,15 +7,27 @@ namespace FindAndReplaceApp.Objects
     public class FindAndReplace
     {
         private string _userString;
+        private string _findWord;
+        private string _replaceWord;
 
-        public FindAndReplace(string newUserString)
+        public FindAndReplace(string newUserString, string newFindWord, string newReplaceWord)
         {
             _userString = newUserString;
+            _findWord = newFindWord;
+            _replaceWord = newReplaceWord;
         }
 
         public string GetUserString()
         {
             return _userString;
+        }
+        public string GetFindWord()
+        {
+            return _findWord;
+        }
+        public string GetReplaceWord()
+        {
+            return _replaceWord;
         }
 
     }
--- Tests/FindAndReplaceTest.cs
+++ Tests/FindAndReplaceTest.cs
@@ -8,15 +8,31 @@ namespace FindAndReplaceAppTest
     public class FindAndReplaceTest
     {
         [Fact]
-        public void FirstTestMethod_Functionality_Result()
+        public void FindAndReplace_Constructor_Result()
         {
             // Arrange
             string userInput = "Hello world";
+            string word1 = "world";
+            string word2 = "universe";
             // Act
-            FindAndReplace newFindAndReplace = new FindAndReplace(userInput);
+            FindAndReplace newFindAndReplace = new FindAndReplace(userInput, word1, word2);
 
             // Assert
             Assert.Equal(userInput, newFindAndReplace.GetUserString());
+            Assert.Equal(word1, newFindAndReplace.GetFindWord());
+            Assert.Equal(word2, newFindAndReplace.GetReplaceWord());
         }
+
+        // [Fact]
+        // public void FindAndReplace_Constructor_Result()
+        // {
+        //     // Arrange
+        //     string userInput = "Hello world";
+        //     // Act
+        //     FindAndReplace newFindAndReplace = new FindAndReplace(userInput);
+        //
+        //     // Assert
+        //     Assert.Equal(userInput, newFindAndReplace.GetUserString());
+        // }
     }
 }
